f00ed3b2585379a2fc2905a2e85f85af24f1539c
Larus Rafn Halldorsson <lallirafn@gmail.com>
2016-05-06 17:51:27 +0000

    Update isx-rates.php
    isx is no longer beta

--- isx-rates.php
+++ isx-rates.php
@@ -1,7 +1,7 @@
 <?php
 
 //get the price from ISX market and store as $iskaur
-$url = 'https://beta.isx.is/api/stats';
+$url = 'https://isx.is/api/stats';
 $json = file_get_contents($url);
 $json = json_decode($json);
 $iskaur = $json->stats->last_price;
